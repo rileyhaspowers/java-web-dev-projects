0184a801e56a15617295bd7c5b044a2b17b37d4f
rileyhaspowers <rileyhaspowers@example.com>
2024-04-01 20:10:54 -0500

    good studio tonight

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/NumericTypes.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+import java.util.Scanner;
+public class NumericTypes {
+    Scanner input1 = new Scanner(System.in);
+    System.out.println("Hey! What's the length of your rectangle?");
+    Scanner input2 = new Scanner(System.in);
+    System.out.println("What about the width?");
+
+    String
+
+}
---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class Circle {
+    public static Double getArea(Double radius){ //is this how you declare a method? public static + the output data type?
+        return 3.14 * radius * radius; // can just return this since it's being grabbed from elsewhere
+    }
+}
